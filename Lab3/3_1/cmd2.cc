9f3ba91ee04660b9418a85ca40dcbba4ee07c854
C-KyS <1912521148@qq.com>
2026-03-22 11:06:48 +0800

    cmd123

--- Lab3/3_1/cmd2.cc
+++ Lab3/3_1/cmd2.cc
@@ -1,2 +1,7 @@
 // myhello
 // 输出"Hello, World!"字符串
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- Lab3/3_1/cmd3.cc
+++ Lab3/3_1/cmd3.cc
@@ -1,3 +1,10 @@
 // mytime
 // 输出当前系统时间
 
+#include <iostream>
+#include <ctime>
+int main() {
+    std::time_t current_time = std::time(nullptr);
+    std::cout << "Current time: " << std::ctime(&current_time);
+    return 0;
+}
